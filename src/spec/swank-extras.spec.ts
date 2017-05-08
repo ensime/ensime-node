e24f0271cf14b11f5b6c744b90e8ceed800fb7ae
Jarlakxen <facundo.m.viale@gmail.com>
2017-05-08 08:37:45 -0300

    Remove unnecessary test

--- src/spec/swank-extras.spec.ts
+++ src/spec/swank-extras.spec.ts
@@ -15,14 +15,4 @@ describe('sexpToJObject', () => {
     expect(result[0][0][0]).toBe('x')
     expect(result[0][1][1]).toBe('Int')
   })
-
-  it('should parse scala notes', () => {
-    const input = `
-        (:scala-notes (:is-full nil :notes ((:file "/Users/viktor/dev/projects/kostbevakningen/src/main/scala/se/kostbevakningen/model/record/Ingredient.scala" :msg "missing
-        arguments for method test in object Ingredient; follow this method with \`_' if you want to treat it as a partially applied function" :severity error :beg 4138 :end 4142 :line 105 :col 3))))
-    `
-
-    const result = sexpToJObject(readFromString(input))
-    expect(result[':scala-notes'][':notes'].length).toBe(1)
-  })
 })
